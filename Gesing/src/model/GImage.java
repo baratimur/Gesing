39017d0614690af86503354b5ee78c6e42493ddc
bara_timur <ra2.xzytech@gmail.com>
2014-02-16 16:51:32 +0700

    implement GImage

--- Gesing/src/model/GImage.java
+++ Gesing/src/model/GImage.java
@@ -4,6 +4,7 @@
  */
 package model;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
@@ -12,16 +13,66 @@ import java.awt.image.WritableRaster;
  *
  * @author Bara Timur
  */
-public class GImage extends BufferedImage {
+public class GImage {
 
-    public GImage(int i, int i1, int i2) {
-        super(i, i1, i2);
+    private BufferedImage bufImage;
+    private int height;
+    private int width;
+    private int RGBArray[][];
+    private Color RGBColorArray[][];
+
+    public GImage(BufferedImage bufImage) {
+        this.bufImage = bufImage;
+        init();
+        fillArray();
     }
 
-    @Override
-    public int[] getRGB(int i, int i1, int i2, int i3, int[] ints, int i4, int i5) {
-        return super.getRGB(i, i1, i2, i3, ints, i4, i5); //To change body of generated methods, choose Tools | Templates.
+    private void init() {
+        height = bufImage.getHeight();
+        width = bufImage.getWidth();
+
+        RGBArray = new int[height][width];
+        RGBColorArray = new Color[height][width];
     }
 
-    
+    private void fillArray() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int color = bufImage.getRGB(i, j);
+                RGBArray[i][j] = color;
+                RGBColorArray[i][j] = new Color(color);
+            }
+        }
+    }
+
+    public BufferedImage getBufImage() {
+        return bufImage;
+    }
+
+    public Color[][] getRGBColorArray() {
+        return RGBColorArray;
+    }
+
+    public Color[] getRGBColorLine(int row) {
+        return RGBColorArray[row];
+    }
+
+    public Color getRGB(int x, int y) {
+        return RGBColorArray[y][x];
+    }
+
+    public void setRGB(int x, int y, Color color) {
+        RGBArray[y][x] = color.getRGB();
+        RGBColorArray[y][x] = color;
+        bufImage.setRGB(x, y, color.getRGB());
+    }
+
+    public void setRGB(int row, Color[] rowColor) {
+        RGBColorArray[row] = rowColor;
+        for (int i = 0; i < width; i++) {
+            Color color =  rowColor[i];
+            RGBArray[row][i] = color.getRGB();
+            bufImage.setRGB(row, i, color.getRGB());
+        }
+    }
 }
